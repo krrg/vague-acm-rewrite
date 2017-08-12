46a6126a8228d0300fea9c74cd6d8f21dbaede99
Ken R <krrgithub@gmail.com>
2017-08-11 23:20:56 -0600

    Working on login screen

--- frontend/components/Login/index.js
+++ frontend/components/Login/index.js
@@ -1,21 +1,25 @@
 import React from "react";
 import * as F from "react-foundation";
 
+import "./Login.scss";
+
 export default class Login extends React.Component {
 
     render() {
         return (
             <div className="Login">
-                <h1>Bank of Brigham</h1>
+                <div className="x-vertical-center">
+                    <h1>Bank of Brigham</h1>
 
-                <form>
-                    <label htmlFor="Login__username">Username</label>
-                    <input type='text' id="Login__username" className="Login__username" placeholder="Username" />
-                    <label htmlFor="Login__password">Password</label>
-                    <input type='password' id="Login__password" className="Login__password" placeholder="Password" />
+                    <form>
+                        <label htmlFor="Login__username">Username</label>
+                        <input type='text' id="Login__username" className="Login__username" placeholder="Username" />
+                        <label htmlFor="Login__password">Password</label>
+                        <input type='password' id="Login__password" className="Login__password" placeholder="Password" />
 
-                    <F.Button>Submit</F.Button>
-                </form>
+                        <F.Button className="Login__submit">Submit</F.Button>
+                    </form>
+                </div>
 
             </div>
         )
